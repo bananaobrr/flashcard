f9e4330524678c7b8ae98fe9c4e3686412d4ef59
bananaobrr <142402700+bananaobrr@users.noreply.github.com>
2024-11-18 08:57:51 -0300

    bm

--- pergunta.js
+++ pergunta.js
@@ -4,18 +4,28 @@ criaCartao(
     'O Python é uma linguagem de programação'
 )
 
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
-
+c
 criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
 criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
